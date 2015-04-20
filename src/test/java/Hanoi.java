62b2a39fb6f6c68c867c3ac67da4d475c63bcd13
zpy <515924628@qq.com>
2015-04-20 12:55:18 +0800

    hanoi

--- src/test/java/Hanoi.java
+++ src/test/java/Hanoi.java
@@ -1,10 +1,10 @@
 public class Hanoi {
 	public static void f(int n, char a, char b, char c) {
 		if (n == 1) {
-			System.out.printf("1移动%c到%c\n", a, c);
+			System.out.printf("移动%c到%c\n", a, c);
 		} else {
 			f(n - 1, a, c, b);
-			System.out.printf("2移动%c到%c\n", a, c);
+			System.out.printf("移动%c到%c\n", a, c);
 			f(n - 1, b, a, c);
 		}
 	}
