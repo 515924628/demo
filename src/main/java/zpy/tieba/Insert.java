d9a8f0a52d5add0206357e71bc464859419a8ca5
zpy <515924628@qq.com>
2015-05-31 19:54:16 +0800

    简单的修改

--- src/main/java/zpy/tieba/Insert.java
+++ src/main/java/zpy/tieba/Insert.java
@@ -1,6 +1,5 @@
 package zpy.tieba;
 
-import org.jsoup.nodes.Element;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import zpy.util.JsoupReq;
@@ -13,7 +12,7 @@ public class Insert {
 
 	public static void main(String[] args) throws IOException {
 		String url = "http://tieba.baidu.com/f/like/furank?kw=java&pn=";
-		String tableName = "java8" + new SimpleDateFormat("MM_dd").format(new Date());
+		String tableName = "java" + new SimpleDateFormat("MM_dd").format(new Date());
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) new ClassPathXmlApplicationContext("jdbc.xml").getBean("jdbcTemplate");
 		jdbcTemplate.update("CREATE TABLE " + tableName + " ( id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL, exp INT NOT NULL, level INT NOT NULL)");
 		for (int i = 1; i <= 5; i++) {
@@ -22,8 +21,8 @@ public class Insert {
 					jdbcTemplate.update(
 					"INSERT INTO " + tableName + "(name, exp, level) VALUES (?,?,?);",
 					item.select("a[username]").html(),
-					item.select(".drl_item_title").select("div").attr("class").substring(5),
-					item.select(".drl_item_exp").select("span").html()
+					item.select(".drl_item_exp").select("span").html(),
+					item.select(".drl_item_title").select("div").attr("class").substring(5)
 			));
 
 //			java7使用
@@ -31,8 +30,8 @@ public class Insert {
 //				jdbcTemplate.update(
 //						"INSERT INTO " + tableName + "(name, exp, level) VALUES (?,?,?);",
 //						item.select("a[username]").html(),
-//						item.select(".drl_item_title").select("div").attr("class").substring(5),
-//						item.select(".drl_item_exp").select("span").html()
+//						item.select(".drl_item_exp").select("span").html(),
+//						item.select(".drl_item_title").select("div").attr("class").substring(5)
 //				);
 //			}
 		}
